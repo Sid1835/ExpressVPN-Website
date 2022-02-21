169558eef2ea9c749f346fd2fe6d6990c4740135
Sid1835 <siddkatkar@gmail.com>
2022-02-21 10:16:01 +0530

    myfirstcode

---
+++ EnvVariablesDeclare.py
@@ -0,0 +1,93 @@
+# my_int = 123
+# my_string = 'Hello, World!'
+# my_flt = 45.06
+# my_bool = 5 > 9  # A Boolean value will return either True or False
+# my_list = ['item_1', 'item_2', 'item_3', 'item_4']
+# my_tuple = ('one', 'two', 'three')
+# my_dict = {'letter': 'g', 'number': 'seven', 'symbol': '&'}
+html_text = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+</body>
+</html>
+"""
+# temp_list = [1, "xxxx", "test"]
+# if __name__ == "__main__":
+#     print(len(html_text))
+    # print(type(len(html_text)))
+    # print(len(temp_list))
+    # print(type(len(temp_list)))
+    # string1 = "katya      "
+    # string2 = """katya2
+    #  sjkdfhsajkfhsdajkhfjdksahfkjda
+    #  dfkdhjfdfhdsgfhjdsgf
+    #  dfjkdshfjkdhsjfdsghfgdhfgjds
+    #  dfjkdshfjdsfhdgsfjhgdsjhfghdsf"""
+    # print(len(string2))
+    # print(type(string2))
+    # result = int(0.9 + 1)
+    # print(result)
+    # my_list = ['item_1', 'item_2', 'item_3', 'item_4']
+    # my_list.append("katya")
+    #
+    # for i, element in enumerate(my_list):
+    #     print(i, element)
+    #
+    # my_tuple = ('Ukraine', 'Kiev', 'Street')
+    # for i, element in enumerate(my_tuple):
+    #     print(i, element)
+    # my_dict = {'key-1': 12345, 'number': 0, 'symbol': '&'}
+    # print(my_dict.keys())
+    # for key in my_dict:
+    #     print(my_dict[key])
+    # print(len(my_dict))
+
+
+
+age = 29
+first_name = 'Sid'
+last_name = 'Katkar'
+myval = 1
+atom_name = 'helium'
+
+# This will Count the alphabets
+html_text: str = "express"
+if __name__ == __name__:
+    print(len(html_text))
+
+# Prints Addition
+print(5 + 3)
+
+# Print Fname and Age
+print(first_name, 'is', age, 'years old')
+print(last_name)
+
+# Print Value
+print(myval)
+
+# Print Age and + years
+print('Age in three years:', age + 3)
+
+# Print First Char
+print(atom_name[0])
+
+# Print Several Chars
+print(atom_name[3:6])
+
+# Print Length
+print(len('helium'))
+
+# Print all letter except last
+print (atom_name [:-1])
+
+# Print last letter except last
+print (atom_name [-1])
+
+# Print 3rd to last letter
+print (atom_name [2:-1])
---
+++ scratch.py
@@ -0,0 +1,36 @@
+age = 29
+first_name = 'Sid'
+last_name = 'Katkar'
+myval = 1
+age = age + 3
+
+atom_name = 'helium'
+
+# This will Count the alphabets
+html_text: str = "express"
+if __name__ == __name__:
+    print(len(html_text))
+
+# Prints Addition
+print(5 + 3)
+
+# Print Fname and Age
+print(first_name, 'is', age, 'years old')
+print(last_name)
+
+# Print Value
+print(myval)
+
+# Print Age and + years
+print('Age in three years:', age)
+
+# Print First Char
+print(atom_name[0])
+
+# Print Several Chars
+print(atom_name[0:3])
+
+# Print Length
+print(len('helium'))
+
+
